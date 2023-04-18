f89cb88359f45d3ad502bf11a73dafcd63e54afd
Harald Melin <haraldme@kth.se>
2023-04-18 12:08:40 +0200

    added small sketch of EM algorithm and with test class

--- src/inference/em.py
+++ src/inference/em.py
@@ -1,14 +1,28 @@
-import hmmlearn
+from hmmlearn import hmm
 
 class EM():
+    """
+    Runs the EM-algorithm for a quadruplet. Requires the copy number sequence of the root and observations of a pair
+    of leaves.
+    """
 
-    def __init__(self, N, M, A, obs):
-        self.N = N
+    def __init__(self, M, A, C_r, obs_1, obs_2):
         self.M = M
         self.A = A
-        self.K = 2*N
-        self.obs = obs
+        self.C_r = C_r
+        self.obs_1 = obs_1
+        self.obs_2 = obs_2
 
     def run(self):
-
+        hmm.PoissonHMM(n_components=self.A,
+                       startprob_prior=1.0,
+                       transmat_prior=1.0,
+                       lambdas_prior=0.0,
+                       lambdas_weight=0.0,
+                       algorithm='viterbi',
+                       random_state=None,
+                       n_iter=10,
+                       tol=0.01,
+                       verbose=False,
+                       params='stl', init_params='stl', implementation='log')
 
---
+++ tests/tests_inference/test_em.py
@@ -0,0 +1,17 @@
+import unittest
+
+import numpy as np
+
+from inference.em import EM
+
+
+class EMTestCase(unittest.TestCase):
+
+    def test_simple_hmm(self):
+        M = 10
+        A = 5
+        C_r = np.zeros((M, A))
+        C_r[:, 2] = 1
+        obs_1 = np.ones(M,) * 2.
+        obs_2 = np.ones(M,) * 2.
+        em = EM(M, A, C_r, obs_1, obs_2)
