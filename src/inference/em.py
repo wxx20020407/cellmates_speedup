f47ffdd4bdc02ae595ebfaf7329182a8a2f9bb7e
Vittorio Zampinetti <zampinetti@gmail.com>
2023-05-31 19:59:07 -0400

    [impl] em runs, maybe correct results. tree runs, but not correct

--- src/inference/em.py
+++ src/inference/em.py
@@ -4,7 +4,8 @@ from typing import Optional, Union
 import numpy as np
 import scipy.stats as stats
 from scipy.special import logsumexp
-from hmmlearn import hmm
+import networkx as nx
+# from hmmlearn import hmm
 
 from models.quadruplet import Quadruplet
 
@@ -93,6 +94,7 @@ def forward_pass(obs_vw, start_prob, trans_mat, log_emissions, eps=1e-5):
 
     # init alpha[0] = start_prob * emission[0] (in log-form)
     alpha[0, ...] = np.log(start_prob.clip(eps)) + log_emissions[0, np.newaxis, ...]
+    alpha[0] -= logsumexp(alpha[0])
 
     for m in range(1, n_sites):
         alpha[m, ...] = logsumexp(alpha[m - 1] +
@@ -108,13 +110,14 @@ def backward_pass(obs_vw, start_prob, trans_mat, log_emissions):
     n_sites = obs_vw.shape[0]
     n_states = trans_mat.shape[0]
     beta = np.empty((n_sites, n_states, n_states, n_states))
-    # initialize beta[M] = 1. (in log form)
-    beta[-1, ...] = 0.
+    # initialize beta[M] = 1. (in log form) - normalized
+    beta[-1, ...] = 3 * np.log(n_states)
+
     # compute iteratively
     for m in reversed(range(n_sites - 1)):
         beta[m, ...] = logsumexp(beta[m + 1, ...] +
                                  np.log(trans_mat)
-                                 + log_emissions[m, np.newaxis, np.newaxis, np.newaxis, ...],
+                                 + log_emissions[m + 1, np.newaxis, np.newaxis, np.newaxis, ...],
                                  axis=(3, 4, 5))
         # normalization step
         beta[m] -= logsumexp(beta[m])
@@ -138,16 +141,18 @@ def two_slice_marginals(obs_vw, epsilon_k, n_states):
     beta = backward_pass(obs_vw, start_prob, trans_mat, log_emissions)
     # tsm shape: (n_sites - 1,) + (n_states,) * 6
     # compute two slice: xi
-    tsm = alpha[(np.arange(n_sites - 1), ...) + (np.newaxis,) * 3] + np.log(trans_mat)[np.newaxis, ...] + \
+    log_xi = alpha[(np.arange(n_sites - 1), ...) + (np.newaxis,) * 3] + np.log(trans_mat)[np.newaxis, ...] + \
         log_emissions[(np.arange(1, log_emissions.shape[0]),) + (np.newaxis,) * 4 + (...,)] + \
         beta[(np.arange(1, beta.shape[0]), ...) + (np.newaxis,) * 3]
-    tsm -= logsumexp(alpha + beta)
+    log_xi -= np.expand_dims(logsumexp(log_xi, axis=tuple(range(1, 7))), axis=tuple(range(1, 7)))
 
-    return np.exp(tsm)
+    assert np.allclose(logsumexp(log_xi, axis=(1, 2, 3, 4, 5, 6)), np.zeros(n_sites - 1))
+
+    return log_xi
 
 
-def update_eps(tsm):
-    n_states = tsm.shape[1]
+def update_eps(log_xi):
+    n_states = log_xi.shape[1]
     eps_new = np.empty(3)
     # get set `A` and `\neg A` as masks on the quadruplet
     # i - i' == j - j' (indexing: `mask[j', j, i', i]`)
@@ -157,17 +162,17 @@ def update_eps(tsm):
     for e in range(3):
         if e == 0:
             # eps_ru
-            pair_tsm = logsumexp(tsm, axis=(0, 2, 3, 5, 6))
+            pair_tsm = logsumexp(log_xi, axis=(0, 2, 3, 5, 6))
             # use comut_mask0
-            deps = logsumexp(pair_tsm[~comut_mask0])
-            deps_prime = logsumexp(pair_tsm[comut_mask0])
+            deps = np.exp(logsumexp(pair_tsm[~comut_mask0]))
+            deps_prime = np.exp(logsumexp(pair_tsm[comut_mask0]))
         else:
             if e == 1:
                 # eps_uv
-                pair_tsm = tsm.sum(axis=(0, 3, 6))
+                pair_tsm = logsumexp(log_xi, axis=(0, 3, 6))
             else:
                 # eps_uw
-                pair_tsm = tsm.sum(axis=(0, 2, 5))
+                pair_tsm = logsumexp(log_xi, axis=(0, 2, 5))
 
             deps = np.exp(logsumexp(pair_tsm[~comut_mask]))
             deps_prime = np.exp(logsumexp(pair_tsm[comut_mask]))
@@ -191,24 +196,54 @@ Implementation of algorithm 6 in write-up
     n_states = 7
     eps_init = (1, 10)
     n_sites, n_cells = obs.shape
-    epsilon_hat = np.empty((n_cells, n_cells))
+    epsilon_hat = - np.ones((n_cells, n_cells))
 
     # for each pair of cells
     for v, w in itertools.combinations(range(n_cells), r=2):
         # initialize eps = (eps_ru, eps_uv, eps_uw)
-        epsilon_k = np.random.beta(1, 10, size=3)
+        epsilon_k = np.random.beta(*eps_init, size=3)
         # triplet state u, v, w
         convergence = False
         while not convergence:
             # compute two slice marginals
-            tsm = two_slice_marginals(obs[:, [v, w]], epsilon_k, n_states)  # shape (n_states, n_states, n_states)
+            # shape (n_sites, n_states x 3, n_states x 3)
+            log_xi = two_slice_marginals(obs[:, [v, w]], epsilon_k, n_states)
             # update epsilon_k
-            epsilon_kp1 = update_eps(tsm)
+            epsilon_kp1 = update_eps(log_xi)
             # check for convergence
-            convergence = np.allclose(epsilon_k, epsilon_kp1)
+            convergence = np.allclose(epsilon_k, epsilon_kp1, rtol=1.e-2)
             # update current eps
-            epsilon_k = epsilon_kp1
+            epsilon_k[...] = epsilon_kp1
 
         epsilon_hat[v, w] = epsilon_k[0]  # ctr distance is eps_ru (first of triplet)
 
     return epsilon_hat
+
+
+def _build_tree_rec(ctr_table, cells: set, edges: set[tuple]):
+
+    if len(cells) == 2:
+        for c in cells:
+            edges.add(('0', f'c{c}'))
+    else:
+        ctr_table_sub = ctr_table[np.ix_(list(cells), list(cells))]
+        v, w = np.argwhere(ctr_table_sub == ctr_table_sub.max())[0].tolist()
+        edges = _build_tree_rec(ctr_table, cells.difference({w}), edges)
+        # find edge with cell v and add split
+        for x, v_ in edges:
+            if v_ == f'c{v}':
+                edges = edges.difference((x, v_))
+                u = x + '_0'
+                edges = edges.union([(x, u), (u, v_), (u, f'c{w}')])
+                break
+
+    return edges
+
+
+def build_tree(ctr_table):
+
+    edges = _build_tree_rec(ctr_table, set(range(ctr_table.shape[0])), set())
+    em_tree = nx.Graph()
+    em_tree.add_edges_from(edges)
+
+    return em_tree
--- tests/tests_inference/test_em.py
+++ tests/tests_inference/test_em.py
@@ -1,22 +1,27 @@
+import itertools
 import unittest
 
+import networkx as nx
 import numpy as np
 
 from inference.em import EM
 from models.quadruplet import Quadruplet
 
-from src.inference.em import em_alg
+from src.inference.em import em_alg, build_tree
 
 
-def _generate_obs():
+def _generate_obs(noise=0):
     # 10 sites, 5 cells
-    return np.array([
+    obs = np.array([
         [200] * 5 + [300] * 5,
         [100] * 5 + [200] * 5,
         [100] * 3 + [200] * 2 + [300] * 5,
         [200] * 10,
         [400] * 2 + [300] * 2 + [200] * 3 + [100] * 3
-    ]).transpose(), np.ones((5, 5))
+    ]).transpose()
+    eps = np.ones((5, 5))
+    noise = np.round(np.random.normal(size=obs.shape) * noise).astype(int)
+    return obs + noise, eps
 
 
 class EMTestCase(unittest.TestCase):
@@ -40,8 +45,23 @@ class EMTestCase(unittest.TestCase):
 
     def test_em_alg(self):
         # generate toy data
-        obs, eps = _generate_obs()
+        obs, eps = _generate_obs(noise=10)
         # run em
         ctr_table = em_alg(obs)
         # assert epsilons
-        print(ctr_table)
+        for v, w in itertools.combinations(range(obs.shape[1]), r=2):
+            print(f"eps({v},{w}) = {ctr_table[v, w]:.3f}")
+            print(np.round((obs[:, [v, w]] / 100)).astype(int).transpose())
+            print(" ------- ")
+        # print(ctr_table)
+
+    def test_tree_inference(self):
+        # generate toy data
+        obs, eps = _generate_obs(noise=10)
+        # run em
+        ctr_table = em_alg(obs)
+        # build tree
+        em_tree = build_tree(ctr_table)
+        print(em_tree)
+        assert nx.is_tree(em_tree)
+
