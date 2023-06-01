0f0e1c76038fa880658edddcd19b6acbcb626ec4
Vittorio Zampinetti <zampinetti@gmail.com>
2023-06-01 15:42:25 -0400

    [note] tree plotting

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
-if __name__=='__main__':
-    obs = gene
+import networkx as nx
+# import matplotlib.pyplot as plt
+# import pydot
+
+from networkx.drawing.nx_pydot import graphviz_layout
+
+from inference.em import em_alg, build_tree
+
+if __name__ == '__main__':
+    print("main not ready yet... exiting program")
+    # # generate toy data
+    # obs, eps = _generate_obs(noise=10)
+    # # run em
+    # ctr_table = em_alg(obs)
+    # # build tree
+    # em_tree = build_tree(ctr_table)
+    # pos = graphviz_layout(em_tree, prog="dot")
+    # nx.draw(em_tree, pos)
+    # plt.show()
+    ## NOTE: code above is an idea for plotting the resulting tree as phylo-tree (with 'dot' option to graphviz)
+
