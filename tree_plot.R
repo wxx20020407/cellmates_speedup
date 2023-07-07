25c293bfda64db93b47ccd159c4f49aadf12e110
Vittorio Zampinetti <zampinetti@gmail.com>
2023-07-07 17:04:56 -0400

    [add] ggtree initial code

---
+++ tree_plot.R
@@ -0,0 +1,30 @@
+library(ggtree)
+library(dplyr)
+library(tibble)
+library(ggplot)
+library(tidyr)
+
+tree <- rtree(5)
+
+cn_matrix <- matrix(c(2,2,2,2,2,3,3,3,3,3,
+               1,1,1,1,1,2,2,2,2,2,
+               1,1,1,2,2,3,3,3,3,3,
+               2,2,2,2,2,2,2,2,2,1,
+               4,4,3,3,2,2,2,1,1,1), nrow = 5)
+colnames(cn_matrix) <- 1:10
+cn_tbl <- cn_matrix %>%
+  as_tibble(col) %>%
+  bind_cols(tibble(id = tree$tip.label)) %>%
+  gather(key = "bin", value = "cn", -id) %>%
+  mutate(bin = as.numeric(bin))
+
+cn_tbl
+
+p <- ggtree(tree)
+
+p2 <- facet_plot(p, panel="cn",
+                 data=cn_tbl,
+                 geom=geom_point, aes(x=bin, fill=cn))
+
+
+p2
