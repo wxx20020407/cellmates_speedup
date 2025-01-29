9702891c029d953d38992c26fa3f5b6c3198cadc
Vittorio Zampinetti <zampinetti@gmail.com>
2025-01-29 20:17:23 +0100

    [fix] untracked files

---
+++ tests/test_models/test_evo_models/test_copy_tree_model.py
@@ -0,0 +1,76 @@
+import unittest
+
+import networkx as nx
+import numpy as np
+
+from models.evolutionary_models.copy_tree import CopyTree
+from models.evolutionary_models import p_delta_change, p_delta_trans_mat, p_delta_start_prob
+
+
+class CopyTreeTestCase(unittest.TestCase):
+    def test_copy_tree_init(self):
+        n_states = 5
+        copytree_model = CopyTree(n_states)
+        self.assertEqual(copytree_model.n_states, n_states)
+        self.assertTrue(copytree_model.eps is None)
+
+    def test_trans_prob(self):
+        n_states = 5
+        eps_init = np.random.rand(3)
+        copytree_model = CopyTree(n_states)
+        copytree_model.eps = eps_init
+        self.assertTrue(np.allclose(copytree_model.eps, eps_init))
+        self.assertEqual(copytree_model.start_prob.shape, (n_states, n_states, n_states))
+        self.assertEqual(copytree_model.trans_mat.shape, (n_states,) * 6)
+        self.assertAlmostEqual(np.sum(copytree_model.start_prob), 1)
+        self.assertTrue(np.allclose(np.sum(copytree_model.trans_mat, axis=(3, 4, 5)), np.ones((n_states,) * 3)))
+
+    def test_p_delta_change(self):
+        n_states = 5
+        # edge cases
+        l = 0
+        p_ddp = p_delta_change(n_states, l, change=True)
+        self.assertAlmostEqual(p_ddp, 0)
+        p_dd = p_delta_change(n_states, l, change=False)
+        self.assertAlmostEqual(p_dd, 1)
+
+        l = np.inf
+        p_ddp = p_delta_change(n_states, l, change=True)
+        self.assertAlmostEqual(p_ddp, 1 / n_states)
+        p_dd = p_delta_change(n_states, l, change=False)
+        self.assertAlmostEqual(p_dd, 1 / n_states)
+
+        l = 5.
+        p_ddp = p_delta_change(n_states, l, change=True)
+        p_dd = p_delta_change(n_states, l, change=False)
+
+        self.assertEqual(p_dd + (n_states - 1) * p_ddp, 1)
+
+    def test_p_delta_trans_mat(self):
+        n_states = 5
+        l = 5.
+        mat = p_delta_trans_mat(n_states, l)
+        self.assertEqual(mat.shape, (n_states, n_states, n_states, n_states))
+        self.assertTrue(np.allclose(np.sum(mat, axis=0), np.ones((n_states,) * 3)))
+
+    def test_p_delta_start_prob(self):
+        n_states = 5
+        l = 5.
+        mat = p_delta_start_prob(n_states, l)
+        self.assertEqual(mat.shape, (n_states, n_states))
+        self.assertTrue(np.allclose(np.sum(mat, axis=0), np.ones(n_states)))
+
+    def test_simulate_cn(self):
+        # TODO: Implement test
+        pass
+
+    def assert_no_transition_from_absorbing_state(self, c, tree):
+        M = c.shape[1]
+        for u, v in tree.edges:
+            for m in range(M):
+                if c[u, m] == 0:
+                    self.assertEqual(
+                        c[v, m],
+                        0,
+                        msg=f"Parent node {u} copy number 0 at m {m} but child has {c[v, m]}",
+                    )
---
+++ tests/test_models/test_evo_models/test_urn_observational_model.py
@@ -0,0 +1,30 @@
+import unittest
+
+import numpy as np
+
+from models.observation_models.urn_observational_model import UrnModel
+
+
+class UrnModelTestCase(unittest.TestCase):
+    # TODO: Find good asserts
+    def test_data_simulation_one_cell_uniform_c(self):
+        """
+        Simulate data and expect x_n to be largest at site with largest gc for large diff of gc_min and gc_max.
+        :return:
+        """
+        N = 1
+        M = 10
+        A = 5
+        c = np.ones((N, M), dtype=int) * 2
+
+        R_0 = 100
+        min_gc = 0.1
+        max_gc = 100.0
+
+        urn_model = UrnModel(N, M, A)
+        x, R, gc, phi = urn_model.simulate_data(R_0, min_gc, max_gc, c)
+
+        gc_max_idx = np.argmax(gc)
+        x_max_idx = np.argmax(x)
+        self.assertEqual(gc_max_idx, x_max_idx, msg="Largest x not at site with largest gc for uniform c.")
+
