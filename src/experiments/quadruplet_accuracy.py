[fix] likelihood computation fixed (faster computation with broadcasting instead of loops)

src/experiments/quadruplet_accuracy.py
@@ -6,7 +6,7 @@ import multiprocessing as mp
 
 from inference.em import EM
 from models.evo import JCBModel
-from models.obs import PoissonModel
+from models.obs import PoissonModel, NormalModel
 from simulation.datagen import simulate_quadruplet, get_ctr_table
 from utils.math_utils import p_from_l, l_from_p
 
@@ -24,7 +24,7 @@ def parse_p_change_list(p_change):
         p_change_w = p_change[2]
     return p_change_u, p_change_v, p_change_w
 
-def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict):
+def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, obs_model):
     i = seed
     print(f"n_sites: {n_sites}")
     print(f"length sizes: {length_size}")
@@ -38,7 +38,7 @@ def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, ga
     print(f"p_change_u: {p_change_u:.5f}, p_change_v: {p_change_v:.5f}, p_change_w: {p_change_w:.5f}")
     length_size_str = '-'.join(length_size)
     evo_model = JCBModel(n_states=n_states)
-    obs_model = PoissonModel(n_states=n_states)
+    # obs_model = PoissonModel(n_states=n_states)
     gamma_params = [gamma_params_dict[s] for s in length_size]
 
     # generate data
@@ -58,10 +58,21 @@ def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, ga
     ctr_table_delta = em.distances - gt_ctr_table
     print(
         f"[{i}] time: {exec_time:.2f}, average delta ctr_table_distance: {np.mean(np.abs(ctr_table_delta[0, 1])):.5f}")
+    obs_model_name = "NA"
+    obs_model_variance = np.nan
+    if isinstance(obs_model, NormalModel):
+        obs_model_name = 'normal'
+        obs_model_variance = 1/obs_model.tau_v_prior
+    elif isinstance(obs_model, PoissonModel):
+        obs_model_name = 'poisson'
+        obs_model_variance = obs_model.lambda_v_prior
+    else:
+        raise ValueError(f"Unknown obs_model: {obs_model}")
     with open(file_name, 'a') as f:
         f.write(
             f"{i},{n_sites},{n_states},{length_size_str},{p_change_u},{p_change_v},{p_change_w},{em.distances[0, 1, 0]},{em.distances[0, 1, 1]},{em.distances[0, 1, 2]},"
-            f"{ctr_table_delta[0, 1, 0]},{ctr_table_delta[0, 1, 1]}," f"{ctr_table_delta[0, 1, 2]},{exec_time:.2f},{em._n_iterations[(0, 1)]},{em._loglikelihoods[(0, 1)]},{true_param_ll}\n")
+            f"{ctr_table_delta[0, 1, 0]},{ctr_table_delta[0, 1, 1]}," f"{ctr_table_delta[0, 1, 2]},{exec_time:.2f},{em._n_iterations[(0, 1)]},{em._loglikelihoods[(0, 1)]},{true_param_ll},"
+            f"{obs_model_name},{obs_model_variance}\n")
 
 
 def main():
@@ -83,6 +94,11 @@ def main():
         # 500,
         # 1000
     ]
+    obs_model_list = [
+        # PoissonModel(n_states=n_states, lambda_v_prior=100),
+        # NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=20),
+        NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=50),
+    ]
     # define a range of p_change values (xs, s, m, l, xl)
     p_change_arr = np.array([0.005, 0.01, 0.05, 0.1, 0.2])
     mean_l = l_from_p(p_change_arr, n_states=n_states)  # mean edge length
@@ -101,13 +117,13 @@ def main():
             [[sizes[4], sizes[2], sizes[0]], [sizes[3], sizes[2], sizes[1]]] # large r->u, medium u->v, small u->w
     )
 
-    num_replicates = 5
+    num_replicates = 1
     print(f"with n_states = {n_states}, alpha = 1.")
     # make filename unique to avoid overwriting
     file_name = f'quadruplet_accuracy_{time.strftime("%y%m%d%H%M%S")}.csv'
     with open(file_name, 'w') as f:
-        f.write('seed,n_sites,n_states,length_params,p_change_u,p_change_v,p_change_w,lu_em,lv_em,lw_em,lu_err,lv_err,lw_err,exec_time,n_iter,loglik,true_ll\n')
-    args = [(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict) for n_sites, length_size, seed in itertools.product(n_sites_list, length_sizes_list, range(num_replicates))]
+        f.write('seed,n_sites,n_states,length_params,p_change_u,p_change_v,p_change_w,lu_em,lv_em,lw_em,lu_err,lv_err,lw_err,exec_time,n_iter,loglik,true_ll,obs_model,obs_var\n')
+    args = [(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, obs_model) for n_sites, length_size, seed, obs_model in itertools.product(n_sites_list, length_sizes_list, range(num_replicates), obs_model_list)]
     if parallel_experiments == 1:
         for arg in args:
             run_experiment(*arg)

src/inference/em.py
@@ -80,9 +80,10 @@ class EM:
         alpha = alpha / (self.n_states - 1) if kwargs.get('jc_correction', False) else alpha
         # init to an average of 5 changes over the whole length if not provided
         p_init_default = 5 / self.n_sites
-        l_init = kwargs.get('l_init', np.array([l_from_p(p_init_default, self.n_states)] * 3) if isinstance(self.evo_model, JCBModel) else np.array([p_init_default] * 3))
+        l_init = np.empty(3)
+        l_init[...] = kwargs.get('l_init', np.array([l_from_p(p_init_default, self.n_states)] * 3) if isinstance(self.evo_model, JCBModel) else np.array([p_init_default] * 3))
         if theta_init is not None:
-            l_init = theta_init
+            l_init[...] = theta_init
 
         l_hat = -np.ones((self.n_cells, self.n_cells, 3))
         zero_tol = kwargs.get('zero_tol', 1e-10)  # saturation level when dp << d (changes are much more prevalent)

src/models/evo/__init__.py
@@ -197,36 +197,38 @@ class EvoModel:
         assert np.allclose(sp.logsumexp(log_xi, axis=(1, 2, 3, 4, 5, 6)), np.zeros(n_sites - 1))
         return log_xi
 
-    def backward_pass(self, obs_vw, log_emissions):
+    def backward_pass(self, obs_vw, log_emissions, normalization=True) -> np.ndarray:
         n_sites = obs_vw.shape[0]
         n_states = self.trans_mat.shape[0]
-        beta = np.empty((n_sites, n_states, n_states, n_states))
-        # initialize beta[M] = 1. (in log form) - normalized
-        beta[-1, ...] = - 3 * np.log(n_states)
+
+        # initialize beta[M] = 1. (in log form)
+        beta = np.zeros((n_sites, n_states, n_states, n_states))
+        if normalization:
+            beta[-1, ...] = - 3 * np.log(n_states)
 
         # compute iteratively
         for m in reversed(range(n_sites - 1)):
             # -- ORIGINAL IMPLEMENTATION --
-            # beta[m, ...] = logsumexp(beta[m + 1, ...] +
-            #                          np.log(trans_mat)
-            #                          + log_emissions[m + 1, np.newaxis, np.newaxis, np.newaxis, ...],
-            #                          axis=(3, 4, 5))
-
-            #  -- ALTERNATIVE IMPLEMENTATION --
-            log_acc = -np.inf * np.ones((n_states, n_states, n_states))
-            for x, y, z in itertools.product(range(n_states), repeat=3):
-                log_acc = np.logaddexp(beta[m + 1, x, y, z] +
-                                       np.log(self.trans_mat[..., x, y, z]) +
-                                       log_emissions[m + 1, y, z],  # ]
-                                       log_acc)
-            beta[m, ...] = log_acc
+            beta[m, ...] = sp.logsumexp(beta[m + 1, None, None, None, :, :, :] +
+                                  np.log(self.trans_mat) +
+                                  log_emissions[m + 1, None, None, None, None, :, :], axis=(3, 4, 5))
+            #  -- ALTERNATIVE IMPLEMENTATION -- (slower but doesn't use broadcasting)
+            # log_acc = -np.inf * np.ones((n_states, n_states, n_states))
+            # for x, y, z in itertools.product(range(n_states), repeat=3):
+            #     log_acc = np.logaddexp(beta[m + 1, x, y, z] +
+            #                            np.log(self.trans_mat[..., x, y, z]) +
+            #                            log_emissions[m + 1, y, z],  # ]
+            #                            log_acc)
+            # beta[m, ...] = log_acc
 
             # normalization step
-            beta[m] -= sp.logsumexp(beta[m])
+            if normalization:
+                beta[m] -= sp.logsumexp(beta[m])
+            # print(f"beta{m} sum: {sp.logsumexp(beta[m])}")
 
         return beta
 
-    def _forward_pass_likelihood(self, obs_vw, log_emissions) -> tuple[np.ndarray, float]:
+    def _forward_pass_likelihood(self, obs_vw, log_emissions, normalization=True) -> tuple[np.ndarray, float]:
         """
         Compute the forward pass of the hidden markov model with three latent chains and return the forward probabilities
         as well as the log likelihood of the observations.
@@ -248,24 +250,32 @@ class EvoModel:
         alpha = np.empty((n_sites, n_states, n_states, n_states))
 
         # init alpha[0] = start_prob * emission[0] (in log-form)
-        alpha[0, ...] = np.log(self.start_prob.clip(eps)) + log_emissions[0, np.newaxis, ...]
-        alpha[0] -= sp.logsumexp(alpha[0])
+        alpha[0, ...] = np.log(self.start_prob.clip(eps)) + log_emissions[0, None, ...]
+        norm = sp.logsumexp(alpha[0])
+        if normalization:
+            alpha[0] -= norm
 
-        log_likelihood = 0.
+        log_likelihood = norm
         for m in range(1, n_sites):
             # [ \sum_{x,y,z} alpha_{m-1}(x, y, z) p(i, j, k | x, y, z) ] p(y_m^{vw} | i, j, k)
-            log_acc = -np.inf * np.ones((n_states, n_states, n_states))
-            for x, y, z in itertools.product(range(n_states), repeat=3):
-                log_acc = np.logaddexp(alpha[m - 1, x, y, z] + np.log(self.trans_mat[x, y, z, ...]), log_acc)
-            alpha[m, ...] = log_acc + log_emissions[m, np.newaxis, ...]
-            # normalization step
+            # -- ORIGINAL IMPLEMENTATION -- (slower)
+            # log_acc = -np.inf * np.ones((n_states, n_states, n_states))
+            # for x, y, z in itertools.product(range(n_states), repeat=3):
+            #     log_acc = np.logaddexp(alpha[m - 1, x, y, z] + np.log(self.trans_mat[x, y, z, ...]), log_acc)
+            # -- ALTERNATIVE IMPLEMENTATION -- (faster, no for loop)
+            log_acc = sp.logsumexp(alpha[m - 1, :, :, :, None, None, None] + np.log(self.trans_mat), axis=(0, 1, 2))
+            alpha[m, ...] = log_acc + log_emissions[m, None, :, :]
             norm = sp.logsumexp(alpha[m])
-            alpha[m] -= norm
+            if normalization:
+                # normalization step
+                alpha[m] -= norm
+
             # update log likelihood to save computation
             log_likelihood += norm
 
-        assert np.allclose(sp.logsumexp(alpha, axis=(1, 2, 3)), np.zeros(n_sites)),\
-            f"Forward pass normalization error:{sp.logsumexp(alpha, axis=(1, 2, 3))}"
+        if normalization:
+            assert np.allclose(sp.logsumexp(alpha, axis=(1, 2, 3)), np.zeros(n_sites)),\
+                f"Forward pass normalization error:{sp.logsumexp(alpha, axis=(1, 2, 3))}"
         return alpha, log_likelihood
 
     @property
@@ -278,6 +288,7 @@ class EvoModel:
     @property
     def trans_mat(self):
         """
+        Indexing: p(x, y, z | i, j, k) = trans_mat[i, j, k, x, y, z]
         array shape (n_states,) * 6
         """
         return self._trans_mat

tests/test_hmm.py
@@ -2,9 +2,11 @@ import itertools
 import unittest
 import numpy as np
 import numpy.testing as npt
+from scipy import special as sp
 
-from models.evo import JCBModel
+from models.evo import JCBModel, CopyTree
 from models.evo.basefunc import p_delta
+from models.obs import PoissonModel, NormalModel
 
 
 class HMMTestCase(unittest.TestCase):
@@ -31,6 +33,37 @@ class HMMTestCase(unittest.TestCase):
             for jj in range(n_states):
                 marg += p_delta(n_states, l, i, ii, j, jj)
             self.assertAlmostEqual(marg, 1.)
+
+    def test_forward_backward(self):
+        # without normalization we have that ll = \sum_{c} \alpha_{t, c} \beta_{t, c} for all t
+        np.random.seed(101)
+        n_sites = 12
+        n_states = 5
+        obs_model = NormalModel(n_states=n_states)
+
+        cnp = np.array([
+            [2] * n_sites,
+            [2] * (n_sites // 2) + [3] * (n_sites // 2),
+            [2] * (n_sites // 2) + [3] * (n_sites // 4) + [4] * (n_sites // 4),
+            [2] * (n_sites // 2) + [3] * (n_sites // 4) + [1] * (n_sites // 4)
+        ])
+        # generate data for two cells
+        obs = obs_model.sample(cnp[2:4])
+
+        # compute with forward-algorithm with normalized forward variables
+        log_emissions = obs_model.log_emission(obs)
+
+        evo_model = CopyTree(n_states=n_states)
+        evo_model.theta = np.array([1/12, 1/12, 1/12])
+
+        _, ll_fwd = evo_model._forward_pass_likelihood(obs, log_emissions, normalization=True)
+        alpha, _ = evo_model._forward_pass_likelihood(obs, log_emissions, normalization=False)
+        beta = evo_model.backward_pass(obs, log_emissions, normalization=False)
+        for t in range(n_sites):
+            ll = sp.logsumexp(alpha[t] + beta[t])
+            # print(f"t={t}, ll: {ll}, ll_fwd: {ll_fwd}")
+            self.assertAlmostEqual(ll, ll_fwd)
+
 
 
 if __name__ == '__main__':

tests/test_inference/test_em.py
@@ -49,12 +49,13 @@ class EMTestCase(unittest.TestCase):
         n_states = 5
         obs, eps = _generate_obs(noise=10)
         # run em
-        em = EM(n_states, PoissonModel(n_states, 100, 100), 'copytree', tree_build='ctr', verbose=2)
-        em.fit(obs, max_iter=30, rtol=1e-3, num_processors=8)
+        evo_model = CopyTree(n_states)
+        em = EM(n_states, PoissonModel(n_states, 100, 100), evo_model, tree_build='ctr', verbose=2)
+        em.fit(obs, max_iter=30, rtol=1e-6, num_processors=1)
         ctr_table = em.distances
         # assert epsilons
         for v, w in itertools.combinations(range(obs.shape[1]), r=2):
-            print(f"eps({v},{w}) = {ctr_table[v, w]:.3f}")
+            print(f"eps({v},{w}) = {ctr_table[v, w]}")
             print(np.round((obs[:, [v, w]] / 100)).astype(int).transpose())
             print(" ------- ")
         # print(ctr_table)
@@ -517,16 +518,17 @@ class EMTestCase(unittest.TestCase):
         np.random.seed(seed)
         n_states = 5
         n_sites = 1000
-        poisson_param = 10
-        normal_param = 1., 10.  # mean and precision
+        poisson_param = 100
+        # normal_param = 1., 20.  # mean and precision
 
-        # obs_model = PoissonModel(n_states, poisson_param, poisson_param)
-        obs_model = NormalModel(n_states, normal_param[0], normal_param[0],
-                                tau_v_prior=normal_param[1], tau_w_prior=normal_param[1])
+        obs_model = PoissonModel(n_states, poisson_param, poisson_param)
+        # obs_model = NormalModel(n_states, normal_param[0], normal_param[0],
+        #                         tau_v_prior=normal_param[1], tau_w_prior=normal_param[1])
         evo_model = CopyTree(n_states=n_states)
 
         data = simulate_quadruplet(n_sites, evo_model=evo_model, obs_model=obs_model,
                                    n_states=n_states, gamma_params=self.DEFAULT_GAMMA_PARAMS)
+        print(data['obs'][data['cn'][1] == 0,1])
         gt_ctr_table = get_ctr_table(data['tree'])  # eps
         # print cn in order r, u, v, w (check simulate_quadruplet doc for sorting info)
         print(f"CN (r, u, v, w):\n{data['cn'][[3, 2, 0, 1], :20]}")
@@ -536,8 +538,8 @@ class EMTestCase(unittest.TestCase):
         print(f"True edge _eps: {gt_ctr_table[0, 1, :].tolist()}")
 
         # run EM
-        em = EM(n_states, obs_model, evo_model)
-        em.fit(data['obs'], max_iter=50, rtol=1e-5, num_processors=1, eps_init=gt_ctr_table[0, 1])
+        em = EM(n_states, obs_model, evo_model, verbose=2)
+        em.fit(data['obs'], max_iter=50, rtol=1e-5, num_processors=1, theta_init=gt_ctr_table[0, 1])
         ctr_table = em.distances
         ll_est = em.loglikelihoods[0, 1]
         # change tree _lengths to match the estimated ones
@@ -558,9 +560,18 @@ class EMTestCase(unittest.TestCase):
         print([c / n_sites for c in copy_number_changes])
 
         # check likelihood
-        ll_true = em.compute_pair_likelihood(data['obs'], theta=gt_ctr_table[0, 1])
+        print(f"(EM) computing likelihood with theta:{ctr_table[0, 1]}")
+        ll_est = em.compute_pair_likelihood(data['obs'], theta=ctr_table[0, 1])
+        print(f"(gen) computing likelihood with theta:{gt_ctr_table[0, 1]}")
+        ll_generating = em.compute_pair_likelihood(data['obs'], theta=gt_ctr_table[0, 1])
+        print(f"(true) computing likelihood with theta:{np.array(copy_number_changes) / n_sites}")
+        ll_true = em.compute_pair_likelihood(data['obs'], theta=np.array(copy_number_changes) / n_sites)
 
-        self.assertGreater(ll_est, ll_true)
+        self.assertGreater(ll_true, ll_generating,
+                           msg="likelihood of eps given known copy numbers should be higher than data-generating eps likelihood")
+        self.assertGreater(ll_est, ll_generating,
+                           msg="likelihood of EM eps values should be higher than data-generating eps likelihood")
+        # self.assertGreater(ll_est, ll_true)
 
         # if these tests don't pass, it's likely that they are wrong
         self.assertAlmostEqual(ctr_table[0, 1, 0], gt_ctr_table[0, 1, 0], delta=0.02)