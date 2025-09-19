90d7132883c33f4a3ba2ec2f13187dacf13dd45b
Vittorio Zampinetti <zampinetti@gmail.com>
2025-09-19 18:04:58 +0200

    [upd] update quadruplet experiment from dev branch

--- reproducibility/experiments/quadruplet_accuracy.py
+++ reproducibility/experiments/quadruplet_accuracy.py
@@ -1,22 +1,17 @@
-"""
-Experiment to test the accuracy of the quadruplet model on simulated data.
-It generates data with a given set of parameters, and then runs the EM algorithm to estimate the parameters.
-Saves the results to a csv file named quadruplet_accuracy_yymmddHHMMSS.csv
-
-Usage: python quadruplet_accuracy.py
-"""
-
+import sys
 import itertools
 import time
 
 import numpy as np
 import multiprocessing as mp
+# import matplotlib.pyplot as plt
 
-from cellmates.inference.em import EM
-from cellmates.models.evo import JCBModel
-from cellmates.models.obs import PoissonModel, NormalModel
-from cellmates.simulation.datagen import simulate_quadruplet, get_ctr_table
-from cellmates.utils.math_utils import p_from_l, l_from_p
+from inference.em import EM
+from models.evo import JCBModel
+from models.obs import PoissonModel, NormalModel
+from simulation.datagen import simulate_quadruplet, get_ctr_table
+from utils.math_utils import p_from_l, l_from_p
+# from utils.visual import plot_cn_profile
 
 
 def parse_p_change_list(p_change):
@@ -32,7 +27,7 @@ def parse_p_change_list(p_change):
         p_change_w = p_change[2]
     return p_change_u, p_change_v, p_change_w
 
-def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, obs_model):
+def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, variance, obs_model):
     i = seed
     print(f"n_sites: {n_sites}")
     print(f"length sizes: {length_size}")
@@ -52,15 +47,21 @@ def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, ga
     # generate data
     data = simulate_quadruplet(n_sites=n_sites, obs_model=obs_model, evo_model=evo_model,
                                gamma_params=gamma_params, seed=i)
+    # fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    # plot_cn_profile(data['cn'].astype(int), ax=ax, title=f"length_params: {length_size_str}")
+    # plt.show()
+    # return True
+
+    # ground truth (data-generating length means)
+    gt_ctr_table = get_ctr_table(data['tree'])
 
     # run EM
     em = EM(n_states=n_states, obs_model=obs_model, evo_model=evo_model, tree_build='ctr')
     start_time = time.time()
-    em.fit(data['obs'], max_iter=max_iter, num_processors=1)
+    # initialize with the true lengths
+    em.fit(data['obs'], max_iter=max_iter, num_processors=1, rtol=1e-8, l_init=gt_ctr_table[0, 1])
     exec_time = time.time() - start_time
 
-
-    gt_ctr_table = get_ctr_table(data['tree'])
     true_param_ll = em.compute_pair_likelihood(data['obs'], theta=gt_ctr_table[0, 1])
 
     ctr_table_delta = em.distances - gt_ctr_table
@@ -79,29 +80,30 @@ def run_experiment(n_sites, length_size, seed, max_iter, file_name, n_states, ga
     with open(file_name, 'a') as f:
         f.write(
             f"{i},{n_sites},{n_states},{length_size_str},{p_change_u},{p_change_v},{p_change_w},{em.distances[0, 1, 0]},{em.distances[0, 1, 1]},{em.distances[0, 1, 2]},"
-            f"{ctr_table_delta[0, 1, 0]},{ctr_table_delta[0, 1, 1]}," f"{ctr_table_delta[0, 1, 2]},{exec_time:.2f},{em._n_iterations[(0, 1)]},{em._loglikelihoods[(0, 1)]},{true_param_ll},"
-            f"{obs_model_name},{obs_model_variance}\n")
+            f"{ctr_table_delta[0, 1, 0]},{ctr_table_delta[0, 1, 1]}," f"{ctr_table_delta[0, 1, 2]},{exec_time:.2f},{em._n_iterations[(0, 1)]},{em._loglikelihoods[(0, 1)]},{true_param_ll},{variance},{obs_model_name},{obs_model_variance}\n")
 
 
 def main():
     # parameters
     max_iter = 60
-    parallel_experiments = 1 # cpus
+    parallel_experiments = 32 # cpus
     n_states = 7
     n_sites_list = [
         200,
-        # 500,
-        # 1000
+        500,
+        1000
     ]
     obs_model_list = [
         # PoissonModel(n_states=n_states, lambda_v_prior=100),
-        # NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=20),
         NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=50),
+        NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=100),
+        NormalModel(n_states=n_states, mu_v_prior=1., tau_v_prior=300),
     ]
     # define a range of p_change values (xs, s, m, l, xl)
     p_change_arr = np.array([0.01, 0.02, 0.05, 0.1, 0.2])
     mean_l = l_from_p(p_change_arr, n_states=n_states)  # mean edge length
-    var_ = 0.0001 * mean_l  # variance
+    base_variance = 0.0001
+    var_ = base_variance * mean_l  # variance
     scale_arr = var_ / mean_l
     shape_arr = mean_l / scale_arr
     gamma_params_dict = [(shape.item(), scale.item()) for shape, scale in zip(shape_arr, scale_arr)]
@@ -109,21 +111,20 @@ def main():
     gamma_params_dict = dict(zip(sizes, gamma_params_dict))
 
     length_sizes_list = (
-            [ [p] for p in sizes ] #+
-            # [[sizes[0], sizes[4]], [sizes[1], sizes[3]] ] + # small r->u, large u->v,w
-            # [[sizes[4], sizes[0]], [sizes[3], sizes[1]] ] + # large r->u, small u->v,w
-            # [[sizes[2], sizes[0], sizes[4]], [sizes[2], sizes[1], sizes[3]]] + # medium r->u, small u->v, large u->w
-            # [[sizes[4], sizes[2], sizes[0]], [sizes[3], sizes[2], sizes[1]]] # large r->u, medium u->v, small u->w
+            [ [p] for p in sizes ] +
+            [[sizes[0], sizes[4]], [sizes[1], sizes[3]] ] + # small r->u, large u->v,w
+            [[sizes[4], sizes[0]], [sizes[3], sizes[1]] ] + # large r->u, small u->v,w
+            [[sizes[2], sizes[0], sizes[4]], [sizes[2], sizes[1], sizes[3]]] + # medium r->u, small u->v, large u->w
+            [[sizes[4], sizes[2], sizes[0]], [sizes[3], sizes[2], sizes[1]]] # large r->u, medium u->v, small u->w
     )
 
-    num_replicates = 1 # number of data replicates per parameter setting
+    num_replicates = 20
     print(f"with n_states = {n_states}, alpha = 1.")
-
     # make filename unique to avoid overwriting
     file_name = f'quadruplet_accuracy_{time.strftime("%y%m%d%H%M%S")}.csv'
     with open(file_name, 'w') as f:
-        f.write('seed,n_sites,n_states,length_params,p_change_u,p_change_v,p_change_w,lu_em,lv_em,lw_em,lu_err,lv_err,lw_err,exec_time,n_iter,loglik,true_ll,obs_model,obs_var\n')
-    args = [(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, obs_model) for n_sites, length_size, seed, obs_model in itertools.product(n_sites_list, length_sizes_list, range(num_replicates), obs_model_list)]
+        f.write('seed,n_sites,n_states,length_params,p_change_u,p_change_v,p_change_w,lu_em,lv_em,lw_em,lu_err,lv_err,lw_err,exec_time,n_iter,loglik,true_ll,variance,obs_model,obs_var\n')
+    args = [(n_sites, length_size, seed, max_iter, file_name, n_states, gamma_params_dict, base_variance, obs_model) for n_sites, length_size, seed, obs_model in itertools.product(n_sites_list, length_sizes_list, range(num_replicates), obs_model_list)]
     if parallel_experiments == 1:
         for arg in args:
             run_experiment(*arg)
@@ -132,6 +133,7 @@ def main():
         with mp.Pool(parallel_experiments) as pool:
             # main loop
             pool.starmap(run_experiment, args)
+    print(f"Results saved in {file_name}")
 
 
 if __name__ == '__main__':
