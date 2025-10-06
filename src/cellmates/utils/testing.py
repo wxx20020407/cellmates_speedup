8bbd3cd618ee4d32758a3725ea6aec1027ce90e3
Harald Melin <haraldme@kth.se>
2025-10-06 14:13:11 +0200

    [feat] enhance create_output_test_folder to support sub-folder naming

--- src/cellmates/utils/testing.py
+++ src/cellmates/utils/testing.py
@@ -3,7 +3,7 @@ import os
 from cellmates import ROOT_DIR
 
 
-def create_output_test_folder() -> str:
+def create_output_test_folder(sub_folder_name=None) -> str:
     """
     Create a test output folder based on the current pytest test name.
     The folder will be created under the "output" directory in the general tests folder.
@@ -16,6 +16,7 @@ def create_output_test_folder() -> str:
     test_dir = test_file.replace('.py', '')
     test_name = test_context[4].split(' ')[0]
     test_name = test_name.replace(']', '').replace('[', '_').replace('/', '_')
+    test_name += '' if sub_folder_name is None else f'/{sub_folder_name}'
     test_folder = os.path.join(ROOT_DIR, 'output', test_dir, test_name)
     if not os.path.exists(test_folder):
         os.makedirs(test_folder)
