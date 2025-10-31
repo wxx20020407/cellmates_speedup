[fix] minor likelihood convergence checks improvement; speedtest likelihood check; time core script + cnasim copy computation if missing

src/cellmates/bin/core.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import logging
+import time
 
 import anndata
 import numpy as np
@@ -10,6 +11,7 @@ from cellmates.inference.neighbor_joining import build_tree
 from cellmates.models.evo import JCBModel
 from cellmates.models.obs import NormalModel
 from cellmates.utils.tree_utils import write_newick
+from cellmates.common_helpers.cnasim_data import correct_readcounts
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -55,8 +57,20 @@ def main():
 
     # set paths
     adata_path = args.input
+    # load data
+    logger.info(f"Reading AnnData file {adata_path}")
+    adata = anndata.read_h5ad(adata_path)
+    if 'copy' not in adata.layers:
+        if 'cnasim-params' in adata.uns:
+            logger.info(f"Found CNAsim parameters in AnnData uns: {adata.uns['cnasim-params']}")
+            logger.info(f"Adding `copy` layer...")
+            correct_readcounts(adata)
+        else:
+            logger.error("No 'copy' layer found in AnnData and no CNAsim parameters in uns to correct readcounts.")
+            raise ValueError("Input AnnData must contain a 'copy' layer or CNAsim parameters for correction.")
+    logger.info(f"Dataset contains {adata.n_obs} cells and {adata.n_vars} bins")
     logger.info(f"Using {args.num_processors} processors for parallel computation")
-    # data info
+    # prepare output paths
     out_path = args.output if args.output else '.'  # default to current directory if not provided
     if out_path:
         os.makedirs(out_path, exist_ok=True)
@@ -65,18 +79,20 @@ def main():
     tree_path = os.path.join(out_path, 'tree.nwk')
     cell_names_path = os.path.join(out_path, 'cell_names.txt')
 
-    # load data
-    logger.info(f"Reading AnnData file {adata_path}")
-    adata = anndata.read_h5ad(adata_path)
-    logger.info(f"Dataset contains {adata.n_obs} cells and {adata.n_vars} bins")
+    # extract observations
     obs, chromosome_ends, cell_names = obs_from_adata(adata)
     logger.debug(f"Excluded {adata.n_obs - obs.shape[1]} normal cells from distance estimation")
     # run inference
+    time_inference = time.time()
     evo_model = JCBModel(n_states=args.n_states, chromosome_ends=chromosome_ends, jc_correction=args.jc_correction, alpha=args.alpha)
     obs_model = NormalModel(n_states=args.n_states, mu_v_prior=1., tau_v_prior=100.)
     em = EM(n_states=args.n_states, evo_model=evo_model, obs_model=obs_model, verbose=args.verbose)
+    # TODO: try with train=True and save obs_model to see which parameters were learned
+    logger.info(f"Starting EM inference with max_iter={args.max_iter}")
     em.fit(obs, max_iter=args.max_iter, num_processors=args.num_processors)
     nx_tree = build_tree(em.distances, edge_attr='branch_length')
+    time_inference_end = time.time()
+    logger.info(f"Inference completed in {time_inference_end - time_inference:.2f} seconds")
     # save results
     np.save(dist_path, em.distances)  # save distance matrix
     nwk_str = write_newick(nx_tree, cell_names=cell_names, out_path=tree_path, edge_attr='branch_length') # save newick tree

src/cellmates/inference/em.py
@@ -72,7 +72,7 @@ class EM:
             self.logger.setLevel(logging.DEBUG)
 
 
-    def fit(self, X: np.ndarray, max_iter: int = 200, rtol: float = 1e-6, num_processors: int = 1,
+    def fit(self, X: np.ndarray, max_iter: int = 200, rtol: float = 1e-4, num_processors: int = 1,
             theta_init=None,
             psi_init=None):
         """
@@ -202,7 +202,7 @@ class EM:
             raise AttributeError("Loglikelihoods not set. Run `fit` or `fit_transform` first.")
         return self._loglikelihoods
 
-def jcb_em_ctrtable(obs: np.ndarray, n_states: int = 7, alpha=1., l_init=None, max_iter: int = 200, rtol: float = 1e-6,
+def jcb_em_ctrtable(obs: np.ndarray, n_states: int = 7, alpha=1., l_init=None, max_iter: int = 200, rtol: float = 1e-4,
                     jc_correction: bool = False, num_processors: int = 1) -> np.ndarray:
     """
     Run the JCB EM algorithm to estimate the centroid-to-root distances for each pair of cells. Wrapper function
@@ -210,7 +210,7 @@ def jcb_em_ctrtable(obs: np.ndarray, n_states: int = 7, alpha=1., l_init=None, m
     """
     return jcb_em_alg(obs, n_states, alpha, l_init, max_iter, rtol, jc_correction, num_processors)['l_hat']
 
-def jcb_em_alg(obs: np.ndarray, n_states: int = 7, alpha=1., l_init=None, max_iter: int = 200, rtol: float = 1e-6,
+def jcb_em_alg(obs: np.ndarray, n_states: int = 7, alpha=1., l_init=None, max_iter: int = 200, rtol: float = 1e-4,
                jc_correction: bool = False, num_processors: int = 1, lam=100) -> dict[str, np.ndarray | dict[tuple[int, int], int | float]]:
     """
 Implementation of JCB EM algorithm in write-up
@@ -240,7 +240,7 @@ Implementation of JCB EM algorithm in write-up
         'loglikelihoods': em.loglikelihoods
     }
 
-def em_alg(obs: np.ndarray, n_states: int = 7, eps_init=None, max_iter: int = 200, rtol: float = 1e-6,
+def em_alg(obs: np.ndarray, n_states: int = 7, eps_init=None, max_iter: int = 200, rtol: float = 1e-4,
            num_processors: int = 1) -> dict[str, np.ndarray | dict[tuple[int, int], int | float]]:
     """
     CopyTree
@@ -270,6 +270,7 @@ def fit_quadruplet(v: int, w: int, obs_vw: np.ndarray,
     It may be used in parallel, so all models are passed as templates and initialized inside the function and the EM
     object is not used here.
     """
+    eps_zero = 1e-12
     logger = logging.getLogger(__name__).getChild(f'fit_quadruplet_{v}_{w}')
     # initialize l = (l_ru, l_uv, l_uw)
     theta_init_ = np.empty(3)  # init desired size
@@ -283,7 +284,7 @@ def fit_quadruplet(v: int, w: int, obs_vw: np.ndarray,
     # compute changes is observation and evolution model specific
     # FIXME: self.E_step_alg can be passed to multi_chr_expected_changes to select algorithm
     diagnostic_data = None
-    d, dp, loglik = None, None, -np.inf
+    d, dp, loglik = None, None, None
     if save_diagnostics:
         diagnostic_data = {'loglikelihoods': [loglik], 'thetas': [theta_init_.copy()], 'psis': [obs_model.psi_array()]}
 
@@ -297,10 +298,11 @@ def fit_quadruplet(v: int, w: int, obs_vw: np.ndarray,
         logger.debug(f"[{it + 1}/{max_iter}] LL = {new_loglik}, d = {d}, dp = {dp}")
 
         # check convergence
-        if new_loglik < loglik:
-            logger.error(f'log likelihood decreased: {new_loglik} < {loglik}')
-        elif (new_loglik - loglik) / np.abs(loglik) < rtol and it > min_iter:
-            convergence = True
+        if loglik is not None:
+            if new_loglik < loglik:
+                logger.error(f'log likelihood decreased: {new_loglik} < {loglik}')
+            elif (new_loglik - loglik) / (np.abs(loglik) + eps_zero) < rtol and it > min_iter:
+                convergence = True
         loglik = new_loglik
 
 